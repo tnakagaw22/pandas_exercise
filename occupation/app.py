7ef73e62094df1933185c8ebc02027d023a64d4e
tomo <tnakagaw22@hotmail.com>
2021-05-11 12:30:57 -0400

    01 added getting & knowning your data Occupation

---
+++ occupation/app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+users = pd.read_csv('data.txt', sep='|')
+users = users.set_index('user_id', drop=True)
+
+# users = pd.read_csv('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user',
+#                       sep='|', index_col='user_id')
+
+types = users.dtypes
+occu_count = users['occupation'].value_counts().count()
+occu_uniq = users.occupation.nunique()
+most_freq = users['occupation'].value_counts().sort_values(ascending=False).head(1)
+
+users.describe(include='all')
+least_freq_age = users.age.value_counts().tail()
+
